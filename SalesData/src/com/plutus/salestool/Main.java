f011c78df05cf5de33bbb7e677fb017c88a57d5c
sren <sren@WL00794.corp.aspiraint.com>
2019-08-15 19:10:56 -0400

    Initial commit of project.

---
+++ SalesData/src/com/plutus/salestool/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.plutus.salestool;
+
+/**
+ * @author sren
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES People!");
+		System.out.println("THIS APP SHOWS SALES DATA!");
+	}
+}
---
+++ SalesData/src/com/plutus/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package com.plutus.salestool;
+
+public class SalesData {
+	private int data[] = {0, 4, 2};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
