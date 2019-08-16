b93d786a91a0cff0dea68e59c6dd7eb363b09f57
sren <sren@WL00794.corp.aspiraint.com>
2019-08-15 21:48:20 -0400

    Added new output message.

--- SalesData/src/com/plutus/salestool/Main.java
+++ SalesData/src/com/plutus/salestool/Main.java
@@ -22,5 +22,6 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES People!");
 		System.out.println("THIS APP SHOWS SALES DATA!");
+		System.out.println("Wow! It pushed!");
 	}
 }
